dd6e87dd1f471d0f07dbe5e04cee48618318c543
joneirikboska <joneirikboska@gmail.com>
2021-04-06 14:33:20 +0200

    Create basic chat app from socket.io example.

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const app = require('express')()
 const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world!</h1>')
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', socket => {
+  console.log('a user connected')
+
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
 })
 
 http.listen(3000, () => {
